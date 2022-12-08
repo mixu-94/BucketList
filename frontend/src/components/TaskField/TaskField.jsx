0129db70d8ab025f3f0f19d051fb86aaa806d9d0
MiXu <michael.schindler894@gmail.com>
2022-12-08 15:45:55 +0100

    taskfield

--- frontend/src/components/TaskField/TaskField.jsx
+++ frontend/src/components/TaskField/TaskField.jsx
@@ -2,28 +2,48 @@ import React from "react";
 import "./TaskField.css";
 import { ImCheckboxChecked, ImCheckboxUnchecked } from "react-icons/im";
 import { useState } from "react";
+import { useEffect } from "react";
 
-const TaskField = (props) => {
-  const [isChecked, setIsChecked] = useState(props.done);
+const TaskField = ({ id, name, description, done, category }) => {
+  const [isChecked, setIsChecked] = useState(done);
+  const [taskcolour, setTaskColour] = useState("green");
 
   function toggleCheck() {
     setIsChecked(!isChecked);
   }
 
+  function setBGColour() {
+    switch (category) {
+      case "Finanzen":
+        setTaskColour("#31d51ca6");
+        break;
+
+      default:
+        setTaskColour("grey");
+        break;
+    }
+  }
+
+  useEffect(() => {
+    setBGColour();
+  }, []);
+
   function openTaskPage() {}
 
   return (
-    <div className="taskfield" key={props.id}>
-      <div className="taskfield-name">{props.name}</div>
+    <div className="taskfield" key={id} style={{ backgroundColor: taskcolour }}>
+      <div className="taskfield-name">
+        <a>{name}</a>
+      </div>
       <div className="taskfield-buttons">
         <div className="taskfield-more">
           <button onClick={openTaskPage}> More</button>
         </div>
         <div className="taskfield-checkmark">
           {isChecked ? (
-            <ImCheckboxChecked size={25} onClick={toggleCheck} />
+            <ImCheckboxChecked size={25} />
           ) : (
-            <ImCheckboxUnchecked size={25} onClick={toggleCheck} />
+            <ImCheckboxUnchecked size={25} />
           )}
         </div>
       </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../components/Main/Nav";
 import Navigation from "../components/Main/Navigation";
 import TaskList from "../components/TaskList/TaskList";
 
 const Home = () => {
+  const [searchkeyword, setSeachKeyWord] = useState();
   // TODO Funktionen für Seitenwechsel
   // useState übergaben
 
   return (
     <>
-      <div className="text-xl text-black font-semibold m-4 border-width: 2px text-center bg-gray-400">
-        <Nav />
+      <div className="text-xl text-black font-semibold m-4 border-width: 2px text-center ">
+        <Nav searchkeyword={searchkeyword} setSeachKeyWord={setSeachKeyWord} />
         <br />
         <br />
         <br />
         <br />
-        <TaskList />
+        <TaskList
+          searchkeyword={searchkeyword}
+          setSeachKeyWord={setSeachKeyWord}
+        />
         <h1>Ich bin die Home</h1>
         <br />
         <br />
